d6f5a7387e2337432617c45ed239a7deca2839c0
Manoela Cirilo <manoelacirilo04@gmail.com>
2021-10-21 20:09:54 -0300

    encriptar senha do usuario

--- main.py
+++ main.py
@@ -10,4 +10,10 @@ p = Phone(phone_number='123', phone_type='Celular')
 # print(a.show_data())
 p2 = Phone(phone_number='456', phone_type='Trabalho')
 u = User(name='Milo', birthdate='2020-01-09', email='milo@gamil.com', password='kida', address=a, phones=[p, p2])
+u.set_password('bola')
 print(u.show_data())
+if u.check_password('bol'):
+    print(f'Bem vindo {u.get_name()}')
+else:
+    print('Não autorizado')
+
